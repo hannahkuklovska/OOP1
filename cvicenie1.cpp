ccff6b9146368eaa8e0666e9cb66e47d5f866c5b
hannahkuklovska <hannah15072002@gmail.com>
2024-09-18 14:53:47 +0200

    struktura produkt

--- cvicenie1.cpp
+++ cvicenie1.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+using namespace std;
+
+struct PRODUKT
+{
+     int ID;
+     string nazov;
+     string vyrobca;
+     int pocet_na_sklade;
+     double cena;
+}
